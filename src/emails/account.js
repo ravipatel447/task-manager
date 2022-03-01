9e440334840a7a00cdfb0829d76f678d7290c263
Ravi Patel <ravipatel@Ravis-MacBook-Air.local>
2022-03-01 09:54:26 +0530

    init commit

---
+++ src/emails/account.js
@@ -0,0 +1,30 @@
+// const sgMail = require('@sendgrid/mail')
+// const sendgridAPIkey = process.env.SENDGRID_API_KEY;
+
+// sgMail.setApiKey(sendgridAPIkey);
+
+// sendingWelcomeEmail = (email,name)=>{
+//     const message = {
+//             to: email,
+//             from: '19dce111@charusat.edu.in',
+//             subject: `welcome ${name} to task manager`,
+//             text: `Hello ${name} welcome to task manager`
+//         }
+//     sgMail.send(message).then((res)=>{console.log("email is successfully sent")}).catch((e)=>{console.log(e);})
+// }
+// sendingDeleteEmail = (email,name)=>{
+//     const message = {
+//             to: email,
+//             from: '19dce111@charusat.edu.in',
+//             subject: `Account deletion of task manager`,
+//             text: `Hello ${name}, Your account with ${email} has been successfully deleted form task manager database`
+//         }
+//     sgMail.send(message).then((res)=>{console.log("email is successfully sent")}).catch((e)=>{console.log(e);})
+// }
+
+// module.exports = {
+//     sendingWelcomeEmail,
+//     sendingDeleteEmail
+// }
+
+                    
---
+++ src/routers/user.js
@@ -0,0 +1,131 @@
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const multer = require('multer');
+// const {sendingWelcomeEmail,sendingDeleteEmail} = require('../emails/account');
+const upload = multer({
+     limits:{
+         fileSize: 1000000
+     },
+     fileFilter(req,file,cb){
+        if(!file.originalname.match(/\.(jpg|jpef|png)$/)){
+            return cb(new Error('please upload word file'))
+        }
+        cb(undefined,true);
+     }
+});
+const sharp = require('sharp');
+const router = new express.Router()
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+    try {
+        await user.save()
+        // sendingWelcomeEmail(user.email,user.name);
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
+})
+
+router.post('/users/me/avatar',auth,upload.single('avatar'),async(req,res)=>{
+    const buffer = await sharp(req.file.buffer).resize({width:250 , height:250}).png().toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send({"success": "your profile pic is uploaded successfully"});
+},(error,req,res,next)=>{
+    res.status(400).send({"error": error.message})
+})
+
+router.patch('/users/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+router.delete('/users/me', auth, async (req, res) => {
+    try {
+        // sendingDeleteEmail(req.user.email,req.user.name);
+        await req.user.remove()
+        res.send(req.user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+router.delete('/users/me/avatar',auth, async (req,res)=>{
+    try {
+        if(!req.user.avatar){
+            throw new Error("image is not available");
+        }
+        req.user.avatar = undefined;
+        await req.user.save();
+        res.send({"success": "your profile pic is deleted successfully"});
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+})
+router.get('/users/:id/avatar',async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        if( !user.avatar){
+            throw new Error("image is not available");
+        }
+        res.set('Content-type','image/png')
+        res.send(user.avatar);
+    }catch(error){
+        res.status(404).send(error.message);
+    }
+})
+
+
+module.exports = router
